3a3e908227835edaa64786f729a620364b67329a
Gabriel Alejandro Soltz <thegaby@gmail.com>
2023-10-11 20:21:28 +0200

    plugin-suggestions

--- semgrep/plugin.go
+++ semgrep/plugin.go
@@ -16,9 +16,9 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			Schema:      ConfigSchema,
 		},
 		TableMap: map[string]*plugin.Table{
-			"semgrep_deployments": tableDeployments(ctx),
-			"semgrep_projects":    tableProjects(ctx),
-			"semgrep_findings":    tableFindings(ctx),
+			"semgrep_deployment": tableDeployments(ctx),
+			"semgrep_project":    tableProjects(ctx),
+			"semgrep_finding":    tableFindings(ctx),
 		},
 	}
 	return p
